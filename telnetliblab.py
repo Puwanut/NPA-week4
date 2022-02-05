3700804f9b6019ca6c9970e37c4e411f923b2c82
Puwanut <friend2000f@gmail.com>
2022-02-05 17:00:38 +0000

    modify telnetliblab.py

--- telnetliblab.py
+++ telnetliblab.py
@@ -2,7 +2,7 @@ import getpass
 import telnetlib
 import time
 
-host = "10.0.15.106"
+host = "172.31.106.4"
 user = input("Enter username: ")
 password = getpass.getpass()
 
@@ -16,9 +16,14 @@ tn.read_until(b"Password:")
 tn.write(password.encode('ascii') + b"\n")
 time.sleep(1)
 
-tn.write(b"show ip int br\n")
-time.sleep(2)
-tn.write(b"exit\n")
+# tn.write(b"show ip int br\n")
+tn.write(b"conf t\n")
+time.sleep(1)
+tn.write(b"int g0/1\n")
+time.sleep(1)
+tn.write(b"ip add 172.31.106.17\n")
+time.sleep(1)
+tn.write(b"no shut\n")
 time.sleep(1)
 
 output = tn.read_very_eager()
